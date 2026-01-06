6ebb86b668f56d5d390a66269ee92c384ae7bbd1
Shikimo <_real_email@example.com>
2026-01-06 16:46:28 +0300

    Начало работы

---
+++ password_manager.py
@@ -0,0 +1,10 @@
+import secrets
+import string
+from datetime import datetime
+PASSWORD_FILE = "password.txt"
+
+def  generate_password(length=16):
+    """Генерирует случайный пароль заданной длины"""
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(secrets.choice(characters) for _ in range(length))
+    return password
