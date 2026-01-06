daf8e3fc32cb0181373dc88fe6b836884cadff36
Shikimo <_real_email@example.com>
2026-01-06 17:57:43 +0300

    .

--- password_manager.py
+++ password_manager.py
@@ -5,7 +5,45 @@ from datetime import datetime
 PASSWORD_FILE = "password.txt"
 
 def  generate_password(length=16):
-    
+
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(secrets.choice(characters) for _ in range(length))
     return password
+def save_entry(login, url, password):
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    line = f"{timestamp} | {url} | {login} | {password}\n"
+    with open(PASSWORD_FILE, "a", encoding="utf=8") as f:
+        f.write(line)
+    print("Запись сохранена в", PASSWORD_FILE)
+
+def main ():
+    print("Генератор паролей")
+    while True:
+     url = input("Введите URL сайта или оставте пустым для выхода").strip()
+     if url == "":
+        print("программа отключена")
+        break
+    login = input("введите логин или электронную почту").strip()
+
+    length_input = input("Задайте длину пароля").strip()
+    if length_input == "":
+       length = 16
+    else:
+       try:
+          length = int(length_input)
+       except ValueError:
+          print("некорректное число, использую длину 16")
+          length = 16  
+    password = generate_password(length)
+    print(f"\nсгенерированный пароль: {password}")
+    save = input("сохранить запись в файл? (y/n):").strip().lower()
+    if save == "y":
+       save_entry(login, url, password)
+    else:
+     print("запись удалена")
+     print("/n---/n")
+
+if __name__ == "_main_":
+   main()
